a7aa50d58bc0a39279558368608fd5a137c323ff
straup <copea@si.edu>
2014-05-07 09:16:13 -0400

    update dependencies; double quote all the things

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
-apt-get install python-numpy python-setuptools
+apt-get install python-numpy python-setuptools python-imaging
+easy_install webcolors colormath RoyGBiv
 
-easy_install gunicorn webcolors colormath RoyGBiv
+# Strictly speaking these aren't necessary but I like them
+
+sudo apt-get install python-gevent
+
+# Ensure we get gunicorn 18.x or higher
+
+easy_install --upgrade gunicorn
