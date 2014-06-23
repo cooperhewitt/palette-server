4243cbae8c7b9cd1205bc4dead28be07ae3f165f
straup <copea@si.edu>
2014-06-23 14:05:34 -0400

    ensure_path

--- flask/server.py
+++ flask/server.py
@@ -6,14 +6,45 @@ import os.path
 import flask
 from flask_cors import cross_origin 
 
+import cooperhewitt.swatchbook
+import roygbiv
+
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
 app = flask.Flask(__name__)
 
-import cooperhewitt.swatchbook
-import roygbiv
-import json
+def build_path(path=None):
+
+    # TO DO: read from config / env
+    root = None
+
+    if not root or not os.path.isdir(root):
+        logging.error("Missing root")
+        flask.abort(500)
+
+    if not path:
+        logging.error("Missing path")
+        flask.abort(400)
+
+    path = flask.safe_join(root, path)
+
+    if not path:
+        logging.error("path considered harmful")
+        flask.abort(400)
+
+    path = os.path.join(root, path)
+    return path
+
+def ensure_path(path=None):
+
+    path = build_path(path)
+
+    if not os.path.exists(path):
+        logging.error("'%s' does not exist" % path)
+        flask.abort(404)
+
+    return path
 
 @app.route('/ping', methods=['GET'])
 @cross_origin(methods=['GET'])
@@ -38,8 +69,7 @@ def get_palette(reference):
     if not path:
         flask.abort(400)
 
-    if not os.path.exists(path):
-        flask.abort(404)
+    path = ensure_path(path)
 
     # Validate path here
 
