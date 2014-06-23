20c803c1120f54bb73415642e060f9fc150ef9e7
straup <straup@gmail.com>
2014-06-23 09:03:56 -0400

    use cooperhewitt.swatchbook – completely untested

--- flask/server.py
+++ flask/server.py
@@ -8,52 +8,9 @@ logging.basicConfig(level=logging.DEBUG)
 
 app = flask.Flask(__name__)
 
+import cooperhewitt.swatchbook
 import roygbiv
-import webcolors
 import json
-import cgi
-import Image
-import logging
-logging.basicConfig(level=logging.DEBUG)
-
-def closest_colour(requested_colour):
-    min_colours = {}
-    
-    for key, name in webcolors.css3_hex_to_names.items():
-        r_c, g_c, b_c = webcolors.hex_to_rgb(key)
-        rd = (r_c - requested_colour[0]) ** 2
-        gd = (g_c - requested_colour[1]) ** 2
-        bd = (b_c - requested_colour[2]) ** 2
-        min_colours[(rd + gd + bd)] = name
-    return min_colours[min(min_colours.keys())]
-
-def get_closest(hex):
-
-    rgb = webcolors.hex_to_rgb(hex)
-    
-    try:
-        closest_name = actual_name = webcolors.rgb_to_name(rgb)
-    except ValueError:
-        closest_name = closest_colour(rgb)
-        actual_name = None
-        
-    if actual_name:
-        actual = webcolors.name_to_hex(actual_name)
-    else:
-        actual = None
-            
-    closest = webcolors.name_to_hex(closest_name)
-
-    return actual, closest
-
-def prep(hex):
-
-    web_actual, web_closest = get_closest(hex)
-    
-    return {
-        'colour': hex,
-        'closest': web_closest,
-    }
 
 @app.route('/ping', methods=['GET'])
 @cross_origin(methods=['GET'])
@@ -61,40 +18,44 @@ def ping():
     rsp = {'stat': 'ok'}
     return flask.jsonify(**rsp)
 
-@app.route('/extract', methods=['GET'])
+@app.route('/extract/<reference>', methods=['GET'])
 @cross_origin(methods=['GET'])
-def get_palette(path):
+def get_palette(reference):
 
-    roy = roygbiv.Roygbiv(path)
-    average = roy.get_average_hex()
-    palette = roy.get_palette_hex()
-    
-    average = prep(average)
-    palette = map(prep, palette)
-    
-    rsp = { 'reference-closest': 'css3', 'average': average, 'palette': palette }
+    reference = "fix-me-palettes/%s.json" % reference
 
-    status = '200 OK'
-    rsp = {}
+    if not os.path.exists(palette):
+        flask.abort(404)
+
+    sb = cooperhewitt.swatchbook.palette(reference)
 
     path = flask.request.args.get('path')
 
     if not path:
         flask.abort(400)
 
-    try:
-        rsp = get_palette(path)
-        rsp['stat']  = 'ok'
-    except Exception, e:
-        flask.abort(500)
-        
-    if rsp['stat'] != 'ok':
-        flask.abort(500)
+    if not os.path.exists(path):
+        flask.abort(404)
 
-    logging.debug("%s : %s" % (path, status))
+    # Validate path here
+
+    roy = roygbiv.Roygbiv(path)
+    average = roy.get_average_hex()
+    palette = roy.get_palette_hex()
+
+    def prep(hex):
+        c_hex, c_name = sb.closest_colour(hex)        
+        return {'color': c_hex, 'closest': closest}
+
+    average = prep(average)
+    palette = map(prep, palette)
+    
+    rsp = {
+        'reference-closest': reference,
+        'average': average, 'palette': palette
+    }
 
     return flask.jsonify(**rsp)
-
 
 if __name__ == '__main__':
     debug = True	# sudo make me a CLI option
